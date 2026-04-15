ce4a72e86cb28e97124268ce2c4feb6f1016b863
lakshitsir <sandhyapatidarlak@gmail.com>
2026-04-15 17:37:29 +0530

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Photo's Vault",
+  description: 'High Quality Photos',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-black text-white`}>{children}</body>
+    </html>
+  )
+    }
+  
